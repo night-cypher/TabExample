0cd4e432078e2b6bf41506b35c7daf06dd3a54f4
HP <krist.ng@corous360.com>
2016-05-27 17:22:22 +0800

    Initial commit

---
+++ app/src/main/java/com/example/questdot/tabexample/SectionsPagerAdapter.java
@@ -0,0 +1,48 @@
+package com.example.questdot.tabexample;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    public SectionsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch(position){
+            case 0:
+                return new PlaceholderFragment();
+            case 1:
+                return new SecondFragment();
+        }
+
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        // Show 3 total pages.
+        return 2;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "SECTION 1";
+            case 1:
+                return "SECTION 2";
+
+        }
+        return null;
+    }
+}
